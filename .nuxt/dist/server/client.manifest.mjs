378189d5ac1aa4a70686ca45226336bdf7be3395
Wen-ace <731564934@qq.com>
2024-03-14 21:33:27 +0800

    chore: add ignore file

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,18 @@
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/Users/acewen/vscode-workplace/materialpro-nuxtjs-free/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "/Users/acewen/vscode-workplace/materialpro-nuxtjs-free/node_modules/nuxt/dist/app/entry.js"
+  }
+}
