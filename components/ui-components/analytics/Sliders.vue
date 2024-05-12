a02c3f26e3f5f1fdde20268ab1888c62d3056eb4
Aisuko <urakiny@gmail.com>
2024-05-12 22:45:07 +1000

    Support Bokeh
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ components/ui-components/analytics/Sliders.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <div v-html="bokehDiv"></div>
+    </div>
+  </template>
+  
+<script>
+import axios from 'axios';
+  
+export default { data() {
+    return {
+        bokehScript: '',
+        bokehDiv: '',
+    };
+},
+    
+async created() {
+    const response = await axios.get('your-api-endpoint');
+    this.bokehScript = response.data.script;
+    this.bokehDiv = response.data.div;
+},
+
+};
+</script>
--- pages/ui-components/Analytics.vue
+++ pages/ui-components/Analytics.vue
@@ -1,7 +1,17 @@
 <script setup lang='ts'>
+import { onMounted } from 'vue';
 import HeatMap from '~/components/ui-components/analytics/HeatMap.vue';
 import Pie from '~/components/ui-components/analytics/Pie.vue';
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.src = 'https://demo.bokeh.org/sliders/autoload.js?bokeh-autoload-element=1000&bokeh-app-path=/sliders&bokeh-absolute-url=https://demo.bokeh.org/sliders';
+  script.id='1000';
+  script.async = true;
+  document.body.appendChild(script);
+});
 </script>
+
 <template>
   <div>
     <div class="w-full">
