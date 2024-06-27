7093efc86a95a29ebff962424b224d6e0975cb65
cbh778899 <cbh778899@outlook.com>
2024-06-27 10:50:20 +1000

    add before & after login menu, functions to be implemented
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ components/Layout/header/UserMenuAfterLogin.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+
+const userMenuItems = ref([
+  {
+    title: "My Profile",
+    desc: "Account Settings",
+  },
+  // {
+  //   title: "My Inbox",
+  //   desc: "Messages & Emails",
+  // },
+  // {
+  //   title: "My Tasks",
+  //   desc: "To-do and Daily Tasks",
+  // },
+]);
+</script>
+
+<template>
+    <v-sheet
+      rounded="md"
+      width="250"
+      elevation="10"
+    >
+      <v-list
+        class="pa-4"
+        elevation="10"
+        rounded="md"
+      >
+        <v-list-item
+          v-for="(item, i) in userMenuItems"
+          :key="i"
+          class="py-2  mb-2"
+          :value="item"
+          :title="item.title"
+          :subtitle="item.desc"
+          rounded="md"
+        />
+        <v-btn
+          block
+          color="primary"
+          class="mt-4 py-4"
+        >
+          Logout
+        </v-btn>
+      </v-list>
+    </v-sheet>
+</template>
---
+++ components/Layout/header/UserMenuBeforeLogin.vue
@@ -0,0 +1,52 @@
+<script setup>
+    const authDetails = ref({
+        username:'',
+        email:'',
+        password:''
+    })
+
+    function emailRules(input) {
+        return (
+            /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+            .test(input) || "Email address invalid!"
+        )
+    }
+</script>
+
+<template>
+    <v-sheet
+      rounded="md"
+      width="300"
+      elevation="10"
+      class="block relative p-4"
+    >
+        <v-text-field 
+            v-model="authDetails.username" 
+            label="Your Username" clearable
+        ></v-text-field>
+        <v-text-field 
+            v-model="authDetails.email" 
+            label="Your Email" type="email" 
+            clearable :rules="[emailRules]"
+        ></v-text-field>
+        <v-text-field 
+            v-model="authDetails.password" 
+            label="Your Password" type="password" 
+            clearable
+        ></v-text-field>
+        <v-btn
+            block rounded="md"
+            color="primary"
+            class="py-4 hover mb-4 relative"
+        >
+            Login
+        </v-btn>
+        <v-btn
+            block rounded="md"
+            color="primary"
+            class="py-4 hover mb-4 relative"
+        >
+            Register
+        </v-btn>
+    </v-sheet>
+</template>
